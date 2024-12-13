ec1a04de27c24b7c4e828cf1e67a1f6544064a1d
sssramail <zion1nice@gmail.com>
2024-12-13 20:50:43 +0900

    Add files via upload

---
+++ c코드(2).cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() 
+{
+/*
+	printf("%s 이고\n","이시온");
+	printf("%d세\n",11);
+	printf("%c형\n",'B');
+	printf("시력은 %.1f , %.1f입니다만???\n",0.7,0.6);
+	
+	printf("나는 %d살 입니다\n",2*10);
+	printf("나의 혈액형은 %c입니다\n",'b');
+	
+	int aaa = 4;
+	int sss = 2;
+	int eee = 16;
+	
+	printf("%d학년 %d반 %d번 이시온 입니다만???\n",aaa,sss,eee);
+	
+	
+	printf("%d년 %d월 알파고 승리\n",2016,3);
+	printf("%d%c프린터로 집짓기\n",3,'D');
+	printf("원주율은 %.2f\n",3.14);
+	printf("%.3fkm 마라톤\n",42.195);
+	
+	int yyy = 2024;
+	int mmm = 12;
+	int ddd = 13;
+	
+	int eee = 3*4/2;
+	
+	float r = 4.5;
+	
+	float aaa = 12.378;
+	
+	printf("%d년 %d월 %d일 입니다람쥐\n",yyy,mmm,ddd);
+	printf("삼각형의 넓이는 %d입니다람쥐\n",eee);
+	printf("원의 둘레는 %.2fcm 이고,원의 넓이는 %.3fcm 입니다람쥐\n",2*r*3.14,r*r*3.14);
+	printf("12378m은 %.3f km 입니다람쥐\n",aaa);
+	*/
+	printf("%s \(약 %c,%c %d년 약%c,%c %d년\)는 고대 그리스의 의사이다.","히포크라테스",'B','C',460,'B','C',370);
+	return 0;
+}
+
