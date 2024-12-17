210de925df9b1457b90140c540ad07522f31aeaa
sssramail <zion1nice@gmail.com>
2024-12-17 18:34:49 +0900

    Add files via upload

---
+++ c코드(3).cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("이시온은\n");
+	printf("천재다\n");
+	printf("왜냐하면\n");
+	printf("천재기 때문이다\n");
+	printf("그런데도 천재가 아니라고 생각한다면\n");
+	printf("당신이 바보인 것이다.\n");
+	printf("그게뭔 ㄱ 소리야?\n");
+	printf("응 ㄱ 소리얌ㅎㅎ\n");
+	printf("ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ");
+	printf("ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ");
+	return 0;
+}
