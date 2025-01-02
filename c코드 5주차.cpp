729dd05637bb494c373389317561fd26f20cbe7d
sssramail <zion1nice@gmail.com>
2025-01-02 16:47:01 +0900

    Add files via upload

---
+++ c코드 5주차.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main()
+{
+/*	int a,b; 
+	
+	a = 23;
+	b = 24;
+	printf("%d\n", a>b);
+	printf("%d\n", a<b);
+	
+	printf("%d\n",10>5&&8<3);
+	printf("%d\n",10>5||8);
+	printf("%d\n",!(10>8));
+	
+	int res;
+	
+	res = 0 && 0;
+	printf("%d\n", res);
+	res = 0 && 1;
+	printf("%d\n", res);
+	res = 1 && 0;
+	printf("%d\n", res);
+	res = 1 && 1;
+	printf("%d\n", res);
+	res = 0 || 0;
+	printf("%d\n", res);
+	res = 0 || 1;
+	printf("%d\n", res);
+	res = 1 || 0;
+	printf("%d\n", res);
+	res = 1 || 1;
+	printf("%d\n", res);
+	
+	printf("%d\n", !res);
+	
+	int n;
+	
+	scanf("%d", &n);
+	printf("%d\n",1 == n%2 || n%5 != 0);
+	printf("%d\n",!1 == n%2 || n%5 == 0);
+	
+	int a;
+	
+	scanf("%d", &a);
+	printf("%d\n",a/7==5);
+	printf("%d\n",a%3==0);
+	printf("%d\n",a%5!=0);
+	printf("%d\n",a%3==0 && a%2==0);
+	printf("%d\n",a%4==0 || a%7==0);
+	
+	printf("%d\n",5000*2>1000*15);
+	printf("%d\n",1000%1>1000%3);
+	printf("%d\n",5000-(500*5+1000*2));
+	printf("%d\n",15000 - 5000 >= 0 || 15000 - 10000 >= 0);*/
+	
+	printf("%d\n",972 - 888 >= 1827 - 888*2);
+	printf("%d\n",80%3 >= 0 > 15 > 100%5 == 0);
+	printf("%d\n",200 + 100 - 250 == 60);
+	
+	return 0;
+}
