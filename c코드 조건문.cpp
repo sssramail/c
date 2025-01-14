201a5328a27b1e6521d23c41d6d3d3cbc79b8b5e
sssramail <zion1nice@gmail.com>
2025-01-14 18:40:55 +0900

    Add files via upload

---
+++ c코드 조건문.cpp
@@ -0,0 +1,132 @@
+#include <stdio.h>
+
+int main() 
+{
+	int a;
+	int b;
+	
+	printf("정수입력:");
+	scanf("%d" ,&a); 
+	printf("규칙입력:");
+	scanf("%d" ,&b);
+	
+	if (b == 1)
+	{
+		a += 5;
+		printf("%d\n",a);
+	}
+	else if (b == 2)
+	{
+		a -= 5;
+		printf("%d\n",a); 
+	}
+	else if (b == 3)
+	{
+		a += 1;
+		printf("%d\n",a);
+	}
+	else if (b == 4)
+	{
+		a -= 1;
+		printf("%d\n",a);
+	}
+	
+	printf("규칙입력:");
+	scanf("%d" ,&b);
+	
+	if (b == 1)
+	{
+		a += 5;
+		printf("%d\n",a);
+	}
+	else if (b == 2)
+	{
+		a -= 5;
+		printf("%d\n",a); 
+	}
+	else if (b == 3)
+	{
+		a += 1;
+		printf("%d\n",a);
+	}
+	else if (b == 4)
+	{
+		a -= 1;
+		printf("%d\n",a);
+	}
+	
+	printf("규칙입력:");
+	scanf("%d" ,&b);
+	
+	if (b == 1)
+	{
+		a += 5;
+		printf("%d\n",a);
+	}
+	else if (b == 2)
+	{
+		a -= 5;
+		printf("%d\n",a); 
+	}
+	else if (b == 3)
+	{
+		a += 1;
+		printf("%d\n",a);
+	}
+	else if (b == 4)
+	{
+		a -= 1;
+		printf("%d\n",a);
+	}
+	
+	printf("규칙입력:");
+	scanf("%d" ,&b);
+	
+	if (b == 1)
+	{
+		a += 5;
+		printf("%d\n",a);
+	}
+	else if (b == 2)
+	{
+		a -= 5;
+		printf("%d\n",a); 
+	}
+	else if (b == 3)
+	{
+		a += 1;
+		printf("%d\n",a);
+	}
+	else if (b == 4)
+	{
+		a -= 1;
+		printf("%d\n",a);
+	}
+	
+	printf("규칙입력:");
+	scanf("%d" ,&b);
+	
+	if (b == 1)
+	{
+		a += 5;
+		printf("%d\n",a);
+	}
+	else if (b == 2)
+	{
+		a -= 5;
+		printf("%d\n",a); 
+	}
+	else if (b == 3)
+	{
+		a += 1;
+		printf("%d\n",a);
+	}
+	else if (b == 4)
+	{
+		a -= 1;
+		printf("%d\n",a);
+	}
+	printf("결과는 %d입니다.",a); 
+	
+	return 0;	
+}
