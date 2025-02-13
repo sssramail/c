5e153987b6fea50601906566a7845006c5dd47c0
sssramail <zion1nice@gmail.com>
2025-02-13 16:51:52 +0900

    Add files via upload

---
+++ c코드 와일.cpp
@@ -0,0 +1,145 @@
+#include <stdio.h>
+
+int main()
+{
+/*	int i = 0;
+	
+	while ( i < 10)
+	{
+		if ( i == 5)
+		{
+			i++;
+			continue;
+		}
+		printf("i의 갑 : %d ",i);
+		i++;
+	}
+	
+	int n = 0;
+	int i = 1;
+	
+	scanf("%d",&n);
+	
+	while (n > 0)
+	{
+		printf("%d ",i);
+		i = i * -1;
+		n = n - 1;
+		
+	} 
+	
+	int n = 0;
+	int i = 0;
+	
+	scanf("%d",&n);
+	
+	while (n > 0)
+	{
+		i += 1;
+		printf("%d ",i%2);
+		n -= 1;
+	}
+	
+	int n = 0;
+	int i = 0;
+	int m = 1;
+	
+	scanf("%d",&n);
+	
+	while (n > 0)
+	{
+		i++;
+		printf("%d ",m*i);
+		m = m * -1;
+		n -= 1;
+	}
+	
+	int n;
+	n = 2;
+	switch(0)
+	{
+		case 0:
+			printf("영입니다 ");
+			break;
+		case 1:
+			printf("일입니다 ");
+			break;
+		case 2:
+			printf("이입니다 ");
+			break;
+		default:
+			printf("다시 입력하시오 ");
+			break;
+	}
+	
+	int n;
+	
+	scanf("%d",&n);
+	
+	switch(n/10)
+	{
+		case 0:
+			printf("유소년");
+			break;
+		case 1:
+			printf("청소년");
+			break;
+		case 2:
+		case 3:
+		printf("청년");
+			break;	
+		case 4:
+		case 5:
+			printf("중년");
+			break;
+		case 6:
+		case 7:
+			printf("장년"); 
+			break;
+		case 8:
+			printf("노년");
+			break;
+		case 9:
+			printf("도전 백세");
+			break;
+		default :
+			printf("성공 백세");
+			break;
+
+	}
+	
+	int n;
+	
+	scanf("%d",&n);
+	
+	while (1)
+	{
+		if (n == 0)
+		{
+			printf("0입니다");
+		}
+		else if (n%2 == 0)
+		{
+			printf("짝수");
+			break;
+		}
+		else if (n%2 == 1)
+		{
+			printf("홀수");
+		}
+		
+	}*/
+	
+	int n;
+	int i;
+	
+	scanf("%d",&n);
+	
+	while (1)
+	{
+		scanf("%d",i);
+		if (i < )
+	}
+	
+	return 0;
+}
