4a988d7625460aa82a6f6fb2032f7f026b38c283
sssramail <zion1nice@gmail.com>
2025-03-11 18:52:02 +0900

    Add files via upload

---
+++ c코드 배열(복습).cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+/*	double e[4];
+	int j = 2;
+	
+	e[0] = 145.7;
+	e[1] = 156.3;
+	e[j] = 147.89;
+	j++;
+	e[j] = 159.4;
+	
+	int ar[10] = {10,20,30,40,50,60,70,80,90,100};
+	int br[10];
+	
+	for (int a = 0;a <= 9;a++)
+	{
+		printf("%d ",ar[a]);
+	}
+	
+	int num[ ] = {5,2,6,7,8,9,4,3,1};
+	int page[10] = {123,57,67,86};
+	int i;
+	for (i = 0;i < 9;i++)
+	{
+		printf("%d ",num[i]);
+	}
+	printf("\n");
+	for (i = 0;i < 10;i++)
+	{
+		printf("%d ",page[i]);
+	}
+	
+	char c;
+	int i;
+	long l;
+	float f;
+	double d;
+	
+	printf("%d %d %d %d %d \n",sizeof(c),sizeof(i),sizeof(l),sizeof(f),sizeof(d));
+	printf("%d %d %d \n",sizeof(char),sizeof(int),sizeof(long));
+	printf("%d %d \n",sizeof(float),sizeof(double));
+	printf("%d %d \n",sizeof(153),sizeof(5.2));#include <stdlib.h>
+	
+	int ar[10];
+	int br[ ] = {1,2,3,4,5};
+	double dr[20];
+	
+	printf("%d %d %d",sizeof(ar),sizeof(br),sizeof(dr));
+	
+	int ar[10];
+	int br[ ] = {1,2,3,4,5,6,7};
+	double dr[20];
+	of(br)/sizeof(int);a++)
+	{
+	for (int a = 0;a < size
+		printf("%d ",br[a]);
+	}*/
+	
+	return 0;	
+}
