3824f0d97b080c3b37ab9a265609be1a21d315ef
sssramail <zion1nice@gmail.com>
2025-03-25 18:54:21 +0900

    Add files via upload

---
+++ c코드 포인터 복습.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+/*	int a;
+	double b;
+	char c;
+	
+	a = 153; b = 12.7; c = 'j';*/
+	
+/*	int a;
+	double b;
+	char c;
+	
+	a = 153; b = 12.7; c = 'j';
+	printf("변수 a의 주소:%d\n",&a);
+	printf("변수 b의 주소:%d\n",&b);
+	printf("변수 c의 주소:%d\n",&c);*/
+	
+/*	int m;
+	
+	printf("%d\n",45);
+	printf("%d\n",'A');
+	printf("%d\n","Coding");
+	printf("%d\n","알고리즘");
+	printf("%d\n",&m);*/
+	
+/*	int n;
+	int *pn,*pr;
+	int ar[10] = {10,20,30,40,50,60,70,80,90,100};
+	
+	n = 49;
+	pn = &n;
+	pr = &ar[2];
+	printf("%d %d %p %p\n",n,ar[2],pn,pr);
+	
+	*pn = 59;
+	*pr = 33;
+	printf("%d %d\n",*pn,*pr);*/
+	
+	
+	
+	return 0;	
+}
