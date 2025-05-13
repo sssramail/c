4c38afd77a1263a95ebc8a710679e9ca8c776a63
sssramail <zion1nice@gmail.com>
2025-05-13 18:49:36 +0900

    Add files via upload

---
+++ c코드 버퍼 문제.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{	
+/*	char ch; //5번 문제 
+	
+	while(1)
+	{
+		scanf("%c",&ch);
+		if (ch == '\n')
+		{
+			break;
+		}
+		printf("%c",ch);
+	}*/
+	
+/*	char ch; //7번 문제 
+	
+	while(1)
+	{
+		scanf("%c",&ch);
+		if (ch == '\n')
+		{
+			break;
+		}
+		if (ch < 97)
+		{
+			printf("%c",ch);
+		}
+	}*/
+	
+/*	char ch; //8번 문제
+	
+	while(1)
+	{
+		scanf("%c",&ch);
+		if (ch == '\n')
+		{
+			break;
+		}
+		if (ch < 97)
+		{
+			printf("%c",ch+32);
+		}
+		else if (ch > 97)
+		{
+			printf("%c",ch-32);
+		}
+	} */
+	
+	return 0;	
+}
