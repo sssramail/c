3c1e0e5f068f1e57d870ee7db048e1650f19458a
sssramail <zion1nice@gmail.com>
2025-05-27 19:01:15 +0900

    Add files via upload

---
+++ c코드 시스템 포지션 복습.cpp
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+/*	char c1 = 'A';
+	char c2 = 'B';
+	
+	printf("%d\n",c1+c2);
+	printf("%d\n",c1 > c2);
+	printf("%d\n",c1 == 'A');
+	printf("%d\n",'b' > c2);
+	printf("%d\n",'a' - c2);*/
+	
+/*	char c;
+	
+	scanf("%c",&c);
+	
+	if (c >= 'A' && c <= 'Z')
+	{
+		printf("1");
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		printf("0");
+	}
+	else
+	{
+		printf("-1");
+	}*/
+	
+/*	char* sp1 = "C3coding";
+	char* sp2 = "C3coding";
+	
+	printf("%d\n",sp1 == sp2);
+	printf("%d\n",sp1 == "C3coding");
+	printf("%d\n",sp2 == "C3coding");
+	printf("%d\n","C3coding" == "C3coding");
+	printf("%d\n",sp1 == "c3coding");
+	
+	printf("%d %d\n",sp1,"c3coding");
+	printf("%d %d\n",sp2,"c3coding");*/
+	
+/*	char str1[ ] = "C3coding";
+	char str2[10] = "C3coding";
+	
+	printf("%d\n",str1 == str2);
+	printf("%d\n",str1 == "C3coding");
+	printf("%d\n",str2 == "C3coding");
+	printf("%d\n","C3coding" == "C3coding");
+	printf("%d\n",str1 == "c3coding");*/
+	
+/*	char *sp = "C3coding";
+	char str[] = "C3coding";
+	int res;
+	
+	res = strcmp(sp,str);
+	printf("%d\n",res);
+	res = strcmp(sp,"C3coding");
+	printf("%d\n",res);
+	res = strcmp("C3coding","C3coding");
+	printf("%d\n",res);
+	
+	res = strcmp("test","text");
+	printf("%d\n",res);
+	res = strcmp("text","test");
+	printf("%d\n",res);*/
+	
+/*	char name1[10];
+	char name2[10];
+	
+	strcpy(name1,"가");
+	strcpy(name2,"나");
+	
+	printf("%d\n",strcmp(name1,name2));
+	printf("%d\n",strcmp("나","가")); */
+	
+/*	char start[15] = "C3";
+	char end[15] = "coding";
+	int len;
+	
+	printf("start = %s %d\n",start,strlen(start));
+	printf("end = %s %d\n",end,strlen(end));
+	
+	strcat(start,end);
+	len = strlen(start);
+	printf("%s %d\n",start,len);*/
+	
+	int *p;
+	
+	*p == 500;
+	
+	printf("%d",*p);
+	
+	return 0;	
+}
